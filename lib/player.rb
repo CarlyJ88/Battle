2b643c732b86de3f5aa2eb263cc3bcc379b0e6d6
Carly <carly.jenkinson65@googlemail.com>
2019-04-18 15:47:47 +0100

    Added hit_points method to return reduced HP of player.

--- lib/player.rb
+++ lib/player.rb
@@ -4,5 +4,10 @@ class Player
 
   def initialize(name)
     @name = name
+    @hit_points = 60
+  end
+
+  def hit_points
+   p @hit_points - 27
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,16 @@
 require 'player'
 describe Player do
   subject(:player1) { Player.new('Carly') }
+  subject(:player2) { Player.new('Dom') }
   describe '#name' do
     it 'should return a name' do
-      expect(subject.name).to eq 'Carly'
+      expect(player1.name).to eq 'Carly'
     end
   end
-end
+
+    describe '#hit_points' do
+      it "reduces player two's HP" do
+        expect(player2.hit_points).to eq 33
+      end
+    end
+  end
